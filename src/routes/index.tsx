c641caa0bf94e37a81f642352492886795c36ebd
Bruno Alves <bruno-alves2010@live.com>
2021-05-07 11:59:52 -0300

    Lazy load

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import Home from '../pages/Home/Home';
-import NotFound from '../pages/NotFound/NotFound';
 import { BrowserRoute } from '../shared/constants/browser-route.const';
+
+const Home = lazy(() => import('pages/Home/Home'));
+const NotFound = lazy(() => import('pages/NotFound/NotFound'));
 
 export default function RoutesContainer() {
     return (
-        <Routes>
-            {/* ADD CART CONTEXT BETWEEN HOME AND CARD ROUTES */}
-            <Route path={BrowserRoute.HOME}>
-                <Route path={BrowserRoute.HOME} element={<Home />} />
-                <Route path={BrowserRoute.HOME_FILTER} element={<Home />} />
-            </Route>
-            <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={<></>}>
+            <Routes>
+                {/* ADD CART CONTEXT BETWEEN HOME AND CARD ROUTES */}
+                <Route path={BrowserRoute.HOME}>
+                    <Route path={BrowserRoute.HOME} element={<Home />} />
+                    <Route path={BrowserRoute.HOME_FILTER} element={<Home />} />
+                </Route>
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Suspense>
     );
 }
